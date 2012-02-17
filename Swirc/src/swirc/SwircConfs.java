6d89d2741f63b904ad80381c4a97eabbd81df8d4
Ville Hämäläinen <ville@mini.pohjola>
2012-02-17 12:27:38 +0200

    Made templates for conf storing classes

---
+++ Swirc/src/swirc/SwircConfs.java
@@ -0,0 +1,12 @@
+package swirc;
+
+/*
+ * Class to store SwircModel configurations
+ * @author Janne Kallunki, Jaakko Ritvanen, Ville Hämäläinen
+ */
+public class SwircConfs {
+
+    public SwircConfs() {
+        //TODO constructor
+    }
+}
