8a3b0496721c0fbdc478935d5e81291e2930bd30
Ville Hämäläinen <ville@mini.pohjola>
2012-03-02 12:54:01 +0200

    Changes to Swirc and added some javadoc

--- Swirc/src/swirc/ConnectDialogController.java
+++ Swirc/src/swirc/ConnectDialogController.java
@@ -3,15 +3,26 @@ package swirc;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+/**
+ * ConnectDialogController provides controller for ConnectDialog. It implements
+ * ActionListener interface.
+ * @auth Janne Kallunki, Ville Hämäläinen, Jaakko Ritvanen
+ */
 public class ConnectDialogController implements ActionListener {
     SwircModel model;
     ConnectDialog view;
     
+    /**
+     *
+     */
     public ConnectDialogController(SwircModel model, ConnectDialog view) {
         this.model = model;
         this.view = view;
     }
 
+    /**
+     *
+     */
     @Override
     public void actionPerformed(ActionEvent e) {
         String code = e.getActionCommand();
--- Swirc/src/swirc/Swirc.java
+++ Swirc/src/swirc/Swirc.java
@@ -1,19 +1,27 @@
 package swirc;
 
 /**
- * Main-class for Swirc IRC-client
+ * Main-class for Swirc IRC-client. Works as a wrapper for components of MVC-model
+ * and also runs the program.
  * @author Janne Kallunki
  */
 public class Swirc {
+   private SwircModel model;
+   private SwircView view;
+   
+   /**
+    *
+    */
+    public Swirc(){
+        this.model = new SwircModel();
+        this.view = new SwircView(model);
+    }
     
     /*
-     * Main method. Wraps MVC-model together.
+     * Main method. Creates instance of Swirc and runs the program.
      * @args Not in use.
      */
     public static void main(String[] args) {
-        SwircModel model = new SwircModel();
-        SwircView view = new SwircView(model);
-        
-        view.setVisible(true);
+        Swirc swirc = new Swirc();     
     }
 }
