7dbc1b4c3cc68ad480bdb96f33777b55fb981706
Ville Hämäläinen <ville@mini.pohjola>
2012-03-02 14:40:02 +0200

    Added again more javadoc

--- Swirc/src/swirc/IrcGateway.java
+++ Swirc/src/swirc/IrcGateway.java
@@ -1,7 +1,16 @@
 package swirc;
 import org.jibble.pircbot.*;
 
+/**
+ * Class to extend abstract class PircBot.
+ * @author Janne Kallunki, Ville Hämäläinen, Jaakko Ritvanen
+ */
 public class IrcGateway extends PircBot {
+
+    /**
+     * Constructor.
+     * @param nick Nickname of the user
+     */
     public IrcGateway(String nick) {
         this.setName(nick);
     }
