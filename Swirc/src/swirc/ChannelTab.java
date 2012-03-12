07e60280d9906775ef8bb6c9979ec900b7ca047d
Janne Kallunki <janne@kallunki.org>
2012-03-12 22:22:56 +0200

    popup menu on user list without functionality

--- Swirc/src/swirc/ChannelTab.java
+++ Swirc/src/swirc/ChannelTab.java
@@ -6,6 +6,9 @@ package swirc;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.util.Observable;
 import java.util.Observer;
 import javax.swing.*;
@@ -18,11 +21,14 @@ public class ChannelTab extends JPanel implements Observer {
     
     private Channel channel;
     private JTextPane messages;
+    private JPopupMenu userMenu;
+    private JList users;
     
     public ChannelTab(Channel c) {
         this.channel = c;
         this.channel.addObserver(this);
         this.setLayout(new BorderLayout());
+        
         
         messages = new JTextPane();
         messages.setText("foobar");
@@ -30,9 +36,43 @@ public class ChannelTab extends JPanel implements Observer {
         JScrollPane msgPane = new JScrollPane(messages);
         this.add(msgPane, BorderLayout.CENTER);
         
-        JList users = new JList(channel.getUsers());
+        users = new JList(channel.getUsers());
         JScrollPane userPane = new JScrollPane(users);
         userPane.setPreferredSize(new Dimension(120, 100));
+        
+        userMenu = new JPopupMenu();
+        
+        JMenuItem item = new JMenuItem("Kick");
+        item.setActionCommand("kick");
+        //item.addActionListener(new PopupListener());
+        userMenu.add(item);
+        
+        item = new JMenuItem("Ban");
+        item.setActionCommand("ban");
+        //item.addActionListener(new PopupListener());
+        userMenu.add(item);
+        
+        MouseListener userMenuListener = new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e)  {
+                trigger(e);
+            }
+            
+            @Override
+            public void mouseReleased(MouseEvent e) {
+                trigger(e);
+            }
+            
+            public void trigger(MouseEvent e) {
+                if (e.isPopupTrigger()) { //if the event shows the menu
+                    users.setSelectedIndex(users.locationToIndex(e.getPoint())); //select the item
+                    userMenu.show(users, e.getX(), e.getY()); //and show the menu
+                }
+            }
+            
+        };
+        users.addMouseListener(userMenuListener);
+        
         this.add(userPane, BorderLayout.EAST);
     }
     
@@ -46,4 +86,6 @@ public class ChannelTab extends JPanel implements Observer {
             messages.setText(channel.getContents());
         }
     }
+    
+    
 }
