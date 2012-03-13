134ea9c902d18785b607521e71942f74b184371f
Janne Kallunki <janne@kallunki.org>
2012-03-13 20:14:46 +0200

    fixed connect dialog cancellation bug

--- Swirc/src/swirc/SwircController.java
+++ Swirc/src/swirc/SwircController.java
@@ -32,12 +32,14 @@ public class SwircController implements ActionListener, Observer {
         String code = e.getActionCommand();
         if(code.equals("connectServer")) {
             HashMap<String,String> con = view.connectPrompt();
-            if(con != null && !con.get("serverAddress").equals("") && !con.get("nick").equals("")) {
-                this.view.addServerView(con.get("serverAddress"));
-                this.model.connect(con.get("serverAddress"), con.get("nick"), con.get("port"), con.get("pasword"));
-            }
-            else {
-                view.showWarning("Your server address or nick was empty!");
+            if(con != null) {
+                if(!con.get("serverAddress").equals("") && !con.get("nick").equals("")) {
+                    this.view.addServerView(con.get("serverAddress"));
+                    this.model.connect(con.get("serverAddress"), con.get("nick"), con.get("port"), con.get("pasword"));
+                }
+                else {
+                    view.showWarning("Your server address or nick was empty!");
+                }
             }
         }
         else if(code.equals("disconnect")) {
